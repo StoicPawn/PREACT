e3da44b988f092bdc61500bf3dd775b70ec4dfb1
StoicPawn <151366316+StoicPawn@users.noreply.github.com>
2026-09-20 07:47:17 +0200

    Export point-in-time replay feature utilities

--- preact/feature_store/__init__.py
+++ preact/feature_store/__init__.py
@@ -1,5 +1,18 @@
 """Feature store utilities for PREACT."""
 from .builder import FeatureStore, aggregate_events, build_feature_store, combine_features
+from .replay_dataset import ReplayDataset, build_replay_dataset
+from .targets import binary_event_target, first_event_time
+from .temporal import entity_feature_frame, entity_feature_snapshot
 
-__all__ = ["FeatureStore", "aggregate_events", "build_feature_store", "combine_features"]
-
+__all__ = [
+    "FeatureStore",
+    "aggregate_events",
+    "build_feature_store",
+    "combine_features",
+    "ReplayDataset",
+    "build_replay_dataset",
+    "binary_event_target",
+    "first_event_time",
+    "entity_feature_frame",
+    "entity_feature_snapshot",
+]
