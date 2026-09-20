070acc5d174f97c326e63d9df7486d693397ad6c
StoicPawn <151366316+StoicPawn@users.noreply.github.com>
2026-09-20 05:47:48 +0200

    Export Wave 1 source connectors

--- preact/history/connectors/__init__.py
+++ preact/history/connectors/__init__.py
@@ -1,6 +1,16 @@
 """Historical source connectors."""
 
 from .base import AcquiredDataset, BulkFileConnector
+from .cow import COWStateSystemConnector
+from .ucdp import UCDPConnector
+from .unhcr import UNHCRConnector
 from .world_bank import WorldBankIndicatorConnector
 
-__all__ = ["AcquiredDataset", "BulkFileConnector", "WorldBankIndicatorConnector"]
+__all__ = [
+    "AcquiredDataset",
+    "BulkFileConnector",
+    "COWStateSystemConnector",
+    "UCDPConnector",
+    "UNHCRConnector",
+    "WorldBankIndicatorConnector",
+]
